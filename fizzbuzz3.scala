6854b19e7a373e962ff8405b7d2e9ba1d3672e27
kon_yu <kon_yu@koozyt.com>
2012-10-10 14:36:21 +0900

    aaa

--- fizzbuzz3.scala
+++ fizzbuzz3.scala
@@ -3,12 +3,12 @@ list.map(fbMes).foreach(println)
 
 def fbMes(i:Int):String={
   if(i%15==0){ 
-    return "FIZZBUZZ"  
+    "FIZZBUZZ"  
   }else if(i%3==0){
-    return "BUZZ" 
+    "BUZZ" 
   }else if(i%5==0){
-    return "FIZZ"
+    "FIZZ"
   }else{
-    return i.toString
+    i.toString
   }
 }
